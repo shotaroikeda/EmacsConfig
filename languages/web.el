04836ae77c37137f8fedddd3e7c188343577f49f
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-08-13 23:15:08 +0900

    disable smartparens for only html files

--- languages/web.el
+++ languages/web.el
@@ -8,3 +8,10 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; Turn off smart parens for only html files
+(defun web-mode-html-conf ()
+  (if (equal (file-name-extension buffer-file-name) "html")
+      (smartparens-mode 0)))
+
+(add-hook 'web-mode-hook 'web-mode-html-conf)
