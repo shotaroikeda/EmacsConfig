6929f4200489af311d085edef7c4e7691ab4ab12
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-08-13 23:15:31 +0900

    removal of redundant code, prevent keyboard bugs

--- macosx.el
+++ macosx.el
@@ -39,15 +39,9 @@
 (exec-path-from-shell-copy-env "PYTHONPATH")
 (exec-path-from-shell-copy-env "PATH")
 
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
 ;; (setq interprogram-cut-function 'paste-to-osx)
 ;; (setq interprogram-paste-function 'copy-from-osx)
+
+;; prevent insertion of phi and other osx special characters
+(set-keyboard-coding-system nil)
 
