36df81c75e2adbec1d085608d5a688975a9ff78f
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-09-13 09:20:47 -0500

    Decreased emacs startup time

--- init.el
+++ init.el
@@ -1,19 +1,24 @@
+;; Set bigger memeory allocation before garbage collection
+(setq gc-cons-threshold 100000000)
+
 ;; Set fresh install to 1 for a fresh install
 (setq fresh-install nil)
 
 ;; Debugging mode
 (setq debug-on-error nil)
 
+
 ;; Must load PATH variables in OSX before initializing packages
-(when (equal system-type 'darwin)
-  (load "~/.emacs.d/macosx.el"))
+(let ((file-name-handler-alist nil))
+  (when (equal system-type 'darwin)
+    (load "~/.emacs.d/macosx.el"))
 
-(load "~/.emacs.d/packages.el")
-(load "~/.emacs.d/general.el")
-(load "~/.emacs.d/custom-funcs.el")
-(load "~/.emacs.d/keybinds.el")
-(load-directory "~/.emacs.d/languages")
+  (load "~/.emacs.d/packages.el")
+  (load "~/.emacs.d/general.el")
+  (load "~/.emacs.d/custom-funcs.el")
+  (load "~/.emacs.d/keybinds.el")
+  (load-directory "~/.emacs.d/languages")
 
-;; Loads other keybinds for mac os dependent of packages
-(when (equal system-type 'darwin)
-  (load "~/.emacs.d/macosx_binds.el"))
+  ;; Loads other keybinds for mac os dependent of packages
+  (when (equal system-type 'darwin)
+    (load "~/.emacs.d/macosx_binds.el")))
