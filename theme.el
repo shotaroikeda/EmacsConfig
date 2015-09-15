965e60440f95fdfc1dde8f36c51f077107161500
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-09-15 12:03:18 -0500

    cleaned up configuration

--- theme.el
+++ theme.el
@@ -3,18 +3,8 @@
 
 (setq curr-bg-color (face-attribute 'default :background))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(linum ((t nil))))
+(custom-set-variables)
+(custom-set-faces '(linum ((t nil))))
 
 (set-face-attribute 'fringe nil :background curr-bg-color :foreground curr-bg-color)
 
@@ -37,17 +27,9 @@
 (require 'powerline)
 (setq powerline-arrow-shape 'space14)
 
-;; (custom-set-faces
-;;  '(mode-line ((t (:foreground "#030303" :background "#FD971F" :box nil))))
-;;  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
-;; (set-face-attribute 'mode-line nil
-;;                     :background "#bdbdbd")
-
 (custom-set-faces
  '(mode-line ((t (:foreground "#030303" :background "#FD971F" :box nil))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+
 (set-face-attribute 'mode-line nil
                     :background "#FD971F")
-
-;; (set-face-attribute 'default nil :font "Meslo for Powerline")
-;; (set-frame-font "Meslo for Powerline" nil t)
