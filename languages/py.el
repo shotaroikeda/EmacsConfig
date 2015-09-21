f1a80ee4c38aa30fa9420df8349229262bb995a6
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-09-20 19:41:38 -0500

    removed elpy and related functions

--- languages/py.el
+++ languages/py.el
@@ -1,8 +1,22 @@
 (require 'python)
-(require 'elpy)
-(elpy-enable)
-(elpy-use-ipython)
 
+(defun my/run-python ()
+  (interactive)
+  (run-python "ipython"))
+
+(defun my/python-shell ()
+  (interactive)
+  (my/run-python)
+  (python-shell-switch-to-shell))
+
+(defun py-load-switch ()
+  (interactive)
+  (python-shell-send-buffer)
+  (python-shell-switch-to-shell))
+
+(add-hook 'python-mode-hook 'eldoc-mode)
+
+(define-key python-mode-map (kbd "C-c C-z") 'my/python-shell)
 ;; Python indentation settings
 (add-hook 'python-mode-hook
           (lambda ()
@@ -15,36 +29,15 @@
         (interactive "p")
         (kmacro-exec-ring-item (quote ([103 103 22 71 73 tab escape] 0 "%d")) arg)))
 
-(defun test-for-lols ()
-  (interactive)
-  (evil-insert-line "k"))
-
-
 (defun my/python-mode-hook ()
   (add-to-list 'company-backends 'company-jedi))
 
 (add-hook 'python-mode-hook 'my/python-mode-hook)
 
-(defun py-load-switch ()
-  (interactive)
-  (python-shell-send-buffer)
-  (elpy-shell-switch-to-shell)
-  )
 
 (define-key python-mode-map (kbd "A-r") 'py-load-switch)
 
-(defun py-open-doc-and-go ()
-  (interactive)
-  (elpy-doc)
-  (other-window 1))
 
-(defun py-open-doc-fast ()
-  (interactive)
-  (elpy-doc))
-
-(define-key python-mode-map (kbd "A-d") 'py-open-doc-fast)
-(define-key python-mode-map (kbd "A-D") 'py-open-doc-and-go)
-;;(define-key python-mode-map (kbd "A-D") '(kill-buffer "*Python Doc*"))
 
 ;; Some smarparens things
 (sp-local-pair 'python-mode "\\\'" "\\\'")
