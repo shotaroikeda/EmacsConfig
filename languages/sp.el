1980a7f0c2ff4979c23f9a50d0b29b522d730c5f
shotaroikeda <ikeda.shot@gmail.com>
2015-12-15 12:24:49 -0600

    completely removed sp mode

--- languages/sp.el
+++
@@ -1,6 +0,0 @@
-(load "~/.emacs.d/languages/sourcepawn-mode-0.1/sourcepawn-mode.el")
-(add-to-list 'auto-mode-alist '(".sp\\'" . sourcepawn-mode))
-(add-to-list 'auto-mode-alist '(".inc\\'" . sourcepawn-mode))
-
-;; Make sure that brackets get inserted with proper indentation
-(sp-local-pair 'sourcepawn-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
