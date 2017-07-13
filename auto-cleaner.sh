b6bf668533d2e9f3fb26a7feb3934765dcd0b40e
shotaroikeda <ikeda2@illinois.edu>
2017-07-13 11:16:27 -0400

    compatiblity for linux bash shells

--- auto-cleaner.sh
+++ auto-cleaner.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-find . -name "*" -depth 1 -not -path "./.git" \
+find . -name '*' -depth 1 -not -path './.git' \
      -exec rm -rf {} +; # Remove all files but the .git to enable resetting
 git reset --hard	# Reset all to reestablish directory structure
